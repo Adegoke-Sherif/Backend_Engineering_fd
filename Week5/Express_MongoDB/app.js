99ba351a8830f3ed52ea2b75b7356d6e898c8f69
Adegoke Sherif <shiffyadegoke@yahoo.com>
2024-03-04 18:06:46 +0100

    third commit

---
+++ Week5/Express_MongoDB/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+require("dotenv").config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home")
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT:http://localhost:${PORT}`)
+})
